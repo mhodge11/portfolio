ca0d6e86ea7ea65257f0abaa65aedc4d8616f219
mhodge11 <micahhodgeutk@icloud.com>
2023-09-28 15:10:07 -0400

    Styling contact form submit button

--- src/components/form-submit.tsx
+++ src/components/form-submit.tsx
@@ -1,13 +1,23 @@
 import { Icons } from "@/components/icons";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { useFormContext } from "react-hook-form";
 
-export function FormSubmit({ children }: { children?: React.ReactNode }) {
+export interface FormSubmitProps {
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export function FormSubmit({ className, children }: FormSubmitProps) {
   const { formState } = useFormContext();
   const { isSubmitting, isLoading } = formState;
 
   return (
-    <Button type="submit" disabled={isSubmitting || isLoading}>
+    <Button
+      type="submit"
+      className={cn(className)}
+      disabled={isSubmitting || isLoading}
+    >
       {(isSubmitting || isLoading) && (
         <Icons.spinner className="mr-2 animate-spin" />
       )}
