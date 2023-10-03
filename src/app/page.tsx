5d82606828fdb0fbec78eae141d8027922b079e4
mhodge11 <micahhodgeutk@icloud.com>
2023-10-03 11:16:11 -0400

    Adding aws experience to bio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,19 +54,20 @@ export default function IndexPage() {
       <div className="px-4 py-8 text-xl">
         Hello, my name is Micah. I am an accomplished Full Stack Software
         Engineer with over seven years of experience, specializing in Swift,
-        JavaScript, TypeScript, and SQL, across various frameworks. My portfolio
-        includes the development of iOS applications, cross-platform
+        JavaScript, TypeScript, GraphQL, and SQL, across various frameworks. My
+        portfolio includes the development of iOS applications, cross-platform
         applications using React Native, as well as web applications utilizing
         React and the more recent Next.js. I am proficient in creating both
         RESTful and GraphQL APIs, often employing Express.js as the foundational
         framework. My database design skills extend to PostgreSQL and MySQL
-        systems. With a strong propensity for self-motivation, I have
-        predominantly functioned as either an independent contributor or as a
-        product lead within compact engineering teams. This experience has
-        equipped me with the problem-solving skills essential for tackling a
-        diverse range of challenges. I relish the opportunity to delve into
-        unfamiliar domains to expand my knowledge base. To discuss potential
-        collaborations, please navigate to the{" "}
+        systems. I possess extensive experience in navigating and utilizing the
+        Amazon Web Services (AWS) ecosystem. With a strong propensity for
+        self-motivation, I have predominantly functioned as either an
+        independent contributor or as a product lead within compact engineering
+        teams. This experience has equipped me with the problem-solving skills
+        essential for tackling a diverse range of challenges. I relish the
+        opportunity to delve into unfamiliar domains to expand my knowledge
+        base. To discuss potential collaborations, please navigate to the{" "}
         {
           <Link
             className="font-semibold underline underline-offset-4"
